eabf6aaecfcd9dc7ad7685810ff2a032993b5b54
achub <a.chubakov005@gmail.com>
2023-09-16 13:40:29 +0200

    Number 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.time.LocalDate;
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("This task done by Chubakov Artem PZ 20 - 1/9");
+
+        LocalDate today = LocalDate.now();
+        System.out.println("Сьогоднішня дата: " + today);
     }
 }
